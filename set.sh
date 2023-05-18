dfd0e9b63b5e36366334dcd727fd5be16ffa9f86
Dennis malware <davspencer30@gmail.com>
2023-05-18 08:48:02 +0300

    added new content

--- set.sh
+++ set.sh
@@ -1,11 +1,11 @@
 # installing all bounty tools on a server
 sudo apt update && sudo apt upgrade
 
-sudo apt install wget git curl htop apache2 net-tools make build-essential
+sudo apt install wget git curl htop apache2 net-tools make build-essential python3-pip whois jq
 
 # installing go 
 wget https://go.dev/dl/go1.20.4.linux-amd64.tar.gz
-rm -rf /usr/local/go && tar -C /usr/local -xzf go1.20.4.linux-amd64.tar.gz
+sudo rm -rf /usr/local/go && sudo tar -C /usr/local -xzf go1.20.4.linux-amd64.tar.gz
 echo 'export GOROOT=/usr/local/go' >> ~/.bashrc
 echo 'export GOPATH=$HOME/go' >> ~/.bashrc
 echo 'export PATH=$GOPATH/bin:$GOROOT/bin:$HOME/.local/bin:$PATH' >> ~/.bashrc 
@@ -30,4 +30,44 @@ echo "installing puredns"
 go install github.com/d3mondev/puredns/v2@latest
 
 echo "installing gauplus"
-$ go install github.com/bp0lr/gauplus@latest
+go install github.com/bp0lr/gauplus@latest
+
+echo "installing amass"
+go install -v github.com/owasp-amass/amass/v3/...@master
+
+echo "installing mapcidr"
+go install -v github.com/projectdiscovery/mapcidr/cmd/mapcidr@latest
+
+echo "installing dnsx"
+go install -v github.com/projectdiscovery/dnsx/cmd/dnsx@latest
+
+echo "installing assetfinder"
+go install github.com/tomnomnom/assetfinder@latest
+
+echo "installing waybackurls"
+go install github.com/tomnomnom/waybackurls@latest
+
+echo "installing"
+go install github.com/gwen001/github-subdomains@latest
+
+echo "installing gitlab subdomains"
+go install github.com/gwen001/gitlab-subdomains@latest
+
+
+echo "installing unfurl"
+go install github.com/tomnomnom/unfurl@latest
+
+echo "installing anew"
+go install -v github.com/tomnomnom/anew@latest
+
+# echo "installing crobat"
+# git clone https://github.com/Cgboal/SonarSearch.git && git cd SonarSearch && make && make install && cd
+
+
+echo "instaalling dns validator"
+git clone https://github.com/vortexau/dnsvalidator.git && cd dnsvalidator/ && pip install -r requirements.txt && sudo python3 setup.py install && cd 
+# && dnsvalidator -tL https://public-dns.info/nameservers.txt -threads 100 -o resolvers.txt
+
+echo "installing certificate enumeration tools"
+git clone https://github.com/UnaPibaGeek/ctfr.git && cd ctfr/ && pip3 install -r requirements.txt &&
+
