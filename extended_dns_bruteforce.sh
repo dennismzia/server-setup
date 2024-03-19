fe693b130ee271c6bcf55e50d7ee3b4c4c4aee9c
Dennis malware <davspencer30@gmail.com>
2024-03-19 11:22:49 +0300

    removed: extended_dns_bruteforce as it was irrelevant

--- extended_dns_bruteforce.sh
+++
@@ -1,4 +0,0 @@
-DIR=$HOME/engagements/$1
-while IFS= read -r DOMAIN; do 
-    puredns bruteforce ~/wordlist/best-dns-wordlist.txt $DOMAIN -r ~/wordlist/resolvers.txt -w subrute.txt --write-wildcards wildcards.txt --write-massdns from_massdns.txt
-done < wildcards.txt
