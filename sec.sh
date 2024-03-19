4542dcf8aab7b117024abb891c55ec4c0160ab6f
Dennis malware <davspencer30@gmail.com>
2024-03-19 11:18:40 +0300

    refactor: removed the background tasks that used up all server resources

--- sec.sh
+++ sec.sh
@@ -1,14 +1,14 @@
 # script for brute forcing second  level subdomains hence the name
 
-max_concurrent=5  # Adjust this value based on your server's resources
+max_concurrent=2  # Adjust this value based on your server's resources
 
 exec 3<$1
 
 counter=1
 while IFS= read -r DOMAIN <&3; do
     {
-        nohup puredns bruteforce /opt/wordlist/best-dns-wordlist.txt "$DOMAIN" -r /opt/wordlist/resolvers.txt -w subrute_${counter}.txt --write-wildcards "wildcards_${counter}.txt" --write-massdns "from_massdns_${counter}.txt" > "sbr2_${counter}.txt"
-    } &
+        puredns bruteforce /opt/wordlist/best-dns-wordlist.txt "$DOMAIN" -r /opt/wordlist/resolvers.txt -w subrute_${counter}.txt --write-wildcards "wildcards_${counter}.txt"  > "sbr2_${counter}.txt"
+    } 
     ((counter++))
 
     # Check if the maximum number of concurrent processes has been reached
