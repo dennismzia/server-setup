cd0c6a45556e1f8b42cd45c3e8da2588e9d19d49
mrobot <davspencer30@gmail.com>
2025-10-03 08:45:45 +0300

    fixed a bug that caused the service to break

--- tst.sh
+++ tst.sh
@@ -1,6 +1,8 @@
 DOMAIN=$1
-DIR=$HOME/engagements/$1
+# DIR=$HOME/engagements/$1
+DIR=$HOME/Documents/engagements/$1
 
-ctfr -d $DOMAIN -o $DIR/ctfr.txt
+# ctfr -d $DOMAIN -o $DIR/ctfr.txt
+# DIR/live.txt was the culprit it had no prefix for dir hehe 
 
-cat $DIR/amas.txt $DIR/ctfr.txt $DIR/subdomains.txt $DIR/wayback2.txt $DIR/wayback.txt | sort -u |  httpx | anew live.txt 
+cat $DIR/amas.txt $DIR/ctfr.txt $DIR/subdomains.txt $DIR/wayback2.txt $DIR/wayback.txt | sort -u |  httpx | anew $DIR/live.txt 
